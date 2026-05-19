7fc3712769864de6ba95aec92be08f499e0a2f11
SADIAMunawar <sadiamunawar446@gmail.com>
2026-05-19 10:41:20 +0500

    feat: implement AES-256-GCM encryption and SHA-384 hashing for file security

--- encryption/encrypt.py
+++ encryption/encrypt.py
@@ -3,7 +3,8 @@ import os
 import hashlib
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
 from config import Config
 import base64
 
